161a66b209c4e2d0eeaef9f65208e5a3efdf2718
出川　大和 <hxdegawa@gmail.com>
2017-02-24 13:31:55 +0900

    Allow to use Markdown syntax

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,6 +2,7 @@ class Memo < ApplicationRecord
   belongs_to :user
 
   def rendered_body
-    
+    markdown = ::Redcarpet::Markdown.new(Redcarpet::Render::HTML.new)
+    markdown.render(body)
   end
 end
